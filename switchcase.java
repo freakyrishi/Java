4e5ec3632fd7928d090b0c6fcbde7fa4752ec3e6
freakyrishi <95204461+freakyrishi@users.noreply.github.com>
2022-07-17 14:39:37 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,23 @@
+
+public class switchcase {
+	public static void main(String[] args) {
+		int ch=3;
+		
+		switch (ch)
+		{
+		case 1:
+			System.out.println("This is number 1");
+			break;
+		case 2:
+			System.out.println("This is number 2");
+			break;
+		case 3:
+			System.out.println("This is number 3");
+			break;
+		default :
+			System.out.println("Wrong Number");
+			break;
+		
+		}
+	}
+} 
